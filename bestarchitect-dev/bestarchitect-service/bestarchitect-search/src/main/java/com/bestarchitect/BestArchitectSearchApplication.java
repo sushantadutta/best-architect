815a751185e8ebdd02898e3eb8d3b9ae84577e60
dutta.sushanta@gmail.com <dutta.sushanta@gmail.com>
2018-02-25 12:54:18 +0530

    Files Added for Search Application

---
+++ bestarchitect-dev/bestarchitect-service/bestarchitect-search/src/main/java/com/bestarchitect/BestArchitectSearchApplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.bestarchitect;
+
+/**
+ * @author Sushanta
+ *
+ */
+public class BestArchitectSearchApplication {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ bestarchitect-dev/bestarchitect-service/bestarchitect-search/src/test/java/com/bestarchitect/BestArchitectSearchApplicationTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.bestarchitect;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author Sushanta
+ *
+ */
+public class BestArchitectSearchApplicationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.bestarchitect.BestArchitectSearchApplication#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
