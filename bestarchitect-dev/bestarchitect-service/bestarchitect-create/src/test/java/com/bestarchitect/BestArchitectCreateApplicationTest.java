233431cf0a78904c5123205b0d9a9f35de1f411b
dutta.sushanta@gmail.com <dutta.sushanta@gmail.com>
2018-02-25 12:14:38 +0530

    Initial Classes Added for Create

---
+++ bestarchitect-dev/bestarchitect-service/bestarchitect-create/src/test/java/com/bestarchitect/BestArchitectCreateApplicationTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.bestarchitect;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author 310214146
+ *
+ */
+public class BestArchitectCreateApplicationTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link com.bestarchitect.BestArchitectCreateApplication#main(java.lang.String[])}.
+	 */
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+}
