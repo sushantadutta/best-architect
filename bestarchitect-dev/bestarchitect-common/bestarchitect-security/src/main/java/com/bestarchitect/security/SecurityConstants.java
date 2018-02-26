365e88a342f2c310bd3c92f8d6ec8564cf4ee1b7
Sushanta Dutta <dutta.sushanta@gmail.com>
2018-02-26 07:52:48 +0530

    Initial Changes

---
+++ bestarchitect-dev/bestarchitect-common/bestarchitect-security/src/main/java/com/bestarchitect/security/SecurityConstants.java
@@ -0,0 +1,8 @@
+package com.bestarchitect.security;
+
+public class SecurityConstants {
+    public static final String HTTPS_PORT = "";
+    public static final String HTTPS_PROTOCOL = "";
+    public static final String HEADER_X_FORWARDED_PROTOCOL = "";
+    public static final String HEADER_X_FORWARDED_PORT = "";
+}
